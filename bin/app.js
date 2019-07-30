cefefb2baf133990bb20ddb1d522cc3cdc18a9f6
kenkz447 <kenkz447@gmail.com>
2019-07-30 12:10:47 +0700

    update app.js to serve fileUrl with query string

--- bin/app.js
+++ bin/app.js
@@ -8,37 +8,46 @@ const app = express();
 
 const publicFolder = path.join(__dirname, 'static');
 
-const getGzipFile = contentType => (req, res, next) => {
-  const gzFile = req.url + '.gz';
-  const fileLocation = path.join(__dirname, gzFile);
-  const gzFileExist = fs.existsSync(fileLocation);
+const getGzipFile = (req, res, next) => {
+    const gzFile = req.path + '.gz';
+    const fileLocation = path.join(__dirname, gzFile);
+    const gzFileExist = fs.existsSync(fileLocation);
 
-  if (!gzFileExist) {
-    return res.sendFile(req.url, { root: __dirname })
-  }
+    if (gzFileExist) {
+        req.url = gzFile;
 
-  req.url = gzFile;
-  res.set('Content-Encoding', 'gzip');
-  res.set('Content-Type', contentType);
-  res.sendFile(req.url, { root: __dirname });
+        if (req._parsedUrl.search) {
+            req.url += req._parsedUrl.search;
+        }
+
+        res.set('Content-Encoding', 'gzip');
+        res.set(
+            'Content-Type',
+            req.path.endsWith('.css.gz')
+                ? 'text/css'
+                : 'text/javascript'
+        );
+    }
+
+    next();
 };
 
 app.get('*/service-worker.js', function (req, res) {
-  res.set('Service-Worker-Allowed', '/');
-  res.sendFile(req.url, { root: __dirname });
+    res.set('Service-Worker-Allowed', '/');
+    res.sendFile(req.url, { root: __dirname });
 });
 
-app.get('*.js', getGzipFile('text/javascript'));
-app.get('*.css', getGzipFile('text/css'));
+app.get('*.js', getGzipFile);
+app.get('*.css', getGzipFile);
 
 app.use('/static', express.static(publicFolder));
 
 app.use(function (req, res) {
-  res.sendFile('index.html', { root: publicFolder });
+    res.sendFile('index.html', { root: publicFolder });
 });
 
 app.set('port', process.env.PORT || 3000);
 
 const server = app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + server.address().port);
+    console.log('Express server listening on port ' + server.address().port);
 });
